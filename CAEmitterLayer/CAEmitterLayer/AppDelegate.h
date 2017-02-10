ff2961a4c56031a2dd18967fa91bdbd90a4fc39c
KarisZhao <347538584@qq.com>
2017-02-10 15:57:25 +0800

    初始化项目

---
+++ CAEmitterLayer/CAEmitterLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CAEmitterLayer
+//
+//  Created by Cavan on 2017/2/10.
+//  Copyright © 2017年 CavanZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CAEmitterLayer/CAEmitterLayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CAEmitterLayer
+//
+//  Created by Cavan on 2017/2/10.
+//  Copyright © 2017年 CavanZhao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
